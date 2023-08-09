b2f6c4da928fc8864ee3e3403ad17194df4718c9
Leonardo do Prado Gomes <108141253+LeoPradoG@users.noreply.github.com>
2023-08-09 17:01:30 -0300

    att
    att

--- dashboard.php
+++ dashboard.php
@@ -12,9 +12,17 @@
 
 <div class="div_br"> </div>
 
+<div style=" width: 100%; text-align: center; font-size: 25px;">
+
+    <b>Visão Geral</b>
+
+</div>
+
+<div class="div_br"> </div>
+
 <div style="width: 100%; min-height: 500px; display: flex; flex-wrap: wrap;">
 
-    <div style="width: 50%; height: 250px; display: flex; justify-content: center; align-items: center; border: solid 1px black;">
+    <div style="width: 50%; height: 250px; display: flex; justify-content: center; align-items: center;">
 
         <div style="width: 75%;">
             <h6>Finalizadas</h6>
@@ -23,11 +31,11 @@
 
     </div>
     
-    <div style="width: 50%; height: 250px; display: flex; justify-content: center; align-items: center; border: solid 1px black;">
+    <div style="width: 50%; height: 250px; display: flex; justify-content: center; align-items: center;">
         
         <div style="width: 75%;">
             <h6>Abertas</h6>
-            <canvas id="chart_os_concluidas"></canvas>
+            <canvas id="chart_os_abertas"></canvas>
         </div>
 
     </div>
@@ -52,32 +60,67 @@
 
 <script>
 
-    const chart_os = document.getElementById('chart_os_concluidas');
+    //DEFININDO CONSTANTES PARA PODER TRABALHAR COM O ELEMENTO
 
-    window.onload = function() {
+        const chart_os = document.getElementById('chart_os_concluidas');
+        const chart_os_abertas = document.getElementById('chart_os_abertas');
 
-        atualizar_grafico_os(); 
+    /////////////////////////////////////////////////////////////////////
 
-    };
+    //CHAMANDO FUNÇÕES PARA SEREM CARREGADAS NA HORA QUE A PAGINA ABRIR
 
-    function atualizar_grafico_os(){
+        window.onload = function() {
 
-        $.ajax({
-            url: 'funcoes/dashboard/ajax_select_os.php',
+            atualizar_grafico_os(); 
+            atualizar_grafico_os_abertas();
 
-            type: 'POST', cache: false,
-            success: function(dataResult){
+        };
 
-                const array = JSON.parse(dataResult);
+    ///////////////////////////////////////////////////////////////////
 
-                create_chart_os(array);
 
-            }
+    //AJAX PARA MANDAR OS DADOS PARA O CHART 
 
-        });
+        function atualizar_grafico_os(){
 
-    }
+            $.ajax({
+                url: 'funcoes/dashboard/ajax_select_os.php',
 
+                type: 'POST', cache: false,
+                success: function(dataResult){
+
+                    const array = JSON.parse(dataResult);
+
+                    create_chart_os(array);
+
+                }
+
+            });
+
+        }
+
+        function atualizar_grafico_os_abertas(){
+
+            $.ajax({
+                url: 'funcoes/dashboard/ajax_select_os_abertas.php',
+
+                type: 'POST', cache: false,
+                success: function(dataResult){
+
+                    const array = JSON.parse(dataResult);
+
+                    create_chart_os_abertas(array);
+
+                }
+
+            });
+
+        }
+
+
+    //////////////////////////////////////////////
+
+    //CRIANDO OS GRAFICOS
 
     function create_chart_os(quantidade){
 
@@ -142,6 +185,75 @@
             }
         });
     }
+
+    function create_chart_os_abertas(quantidade){
+
+        console.log(quantidade);
+        const coresMeses = [
+
+            'rgba(255, 99, 132, 0.8)', // Jan
+            'rgba(54, 162, 235, 0.8)', // Fev
+            'rgba(255, 206, 86, 0.8)', // Mar
+            'rgba(75, 192, 192, 0.8)', // Abr
+            'rgba(153, 102, 255, 0.8)', // Mai
+            'rgba(255, 159, 64, 0.8)', // Jun
+            'rgba(255, 99, 132, 0.8)', // Jul
+            'rgba(54, 162, 235, 0.8)', // Ago
+            'rgba(255, 206, 86, 0.8)', // Set
+            'rgba(75, 192, 192, 0.8)', // Out
+            'rgba(153, 102, 255, 0.8)', // Nov
+            'rgba(255, 159, 64, 0.8)', // Dez
+
+        ];
+
+        new Chart(chart_os_abertas, {
+            type: 'bar',
+            data: {
+
+                labels: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+                datasets: [
+                    {
+                        //label: 'Finalizadas',
+                        data: quantidade,
+                        backgroundColor: coresMeses,
+                        borderWidth: 1
+                    }
+                ]
+            },
+
+            options: {
+
+                scales: {
+
+                    y: {
+                        ticks: {
+                            stepSize: 1, // Display integer values on Y-axis
+                            precision: 0 // Display integer values without decimals
+                        }
+                    }
+
+                },
+
+                indexAxis: 'x',
+                elements: {
+                    bar: {
+                        borderWidth: 2,
+                    }
+                },
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    display: false // Oculta a legenda
+                    }
+                }
+            }
+        });
+
+    }
+
+
+    //////////////////////////////////////////////////////////
 
 </script>
 
--- funcoes/dashboard/ajax_select_os_abertas.php
+++ funcoes/dashboard/ajax_select_os_abertas.php
@@ -7,18 +7,19 @@
                 FROM (
                 SELECT DISTINCT EXTRACT(MONTH FROM atd.DT_ATENDIMENTO) AS MES
                 FROM dbamv.ATENDIME atd
-                WHERE EXTRACT(YEAR FROM atd.DT_ATENDIMENTO) = EXTRACT(YEAR FROM SYSDATE)
+                WHERE EXTRACT(YEAR FROM atd.DT_ATENDIMENTO) = EXTRACT(YEAR FROM SYSDATE) 
                 ORDER BY EXTRACT(MONTH FROM atd.DT_ATENDIMENTO) ASC) atd_mes
 
-                LEFT JOIN(SELECT EXTRACT(MONTH FROM solmv.DT_EXECUCAO) AS MES,
-                            COUNT(sol.CD_SOLICITACAO) AS QTD
-                    FROM nucleoinfo.SOLICITACAO sol
-                    INNER JOIN dbamv.SOLICITACAO_OS solmv
-                        ON solmv.CD_OS = sol.CD_OS_MV 
-                    WHERE solmv.TP_SITUACAO = 'C'
-                    GROUP BY EXTRACT(MONTH FROM solmv.DT_EXECUCAO))qtd_sol
+                LEFT JOIN(SELECT EXTRACT(MONTH FROM solmv.DT_PEDIDO) AS MES,
+                        COUNT(sol.CD_SOLICITACAO) AS QTD
+                FROM nucleoinfo.SOLICITACAO sol
+                INNER JOIN dbamv.SOLICITACAO_OS solmv
+                    ON solmv.CD_OS = sol.CD_OS_MV 
+                GROUP BY EXTRACT(MONTH FROM solmv.DT_PEDIDO)
+                
+                )qtd_sol
                 ON atd_mes.MES = qtd_sol.MES
-                ORDER BY atd_mes.MES ASC ";
+                ORDER BY atd_mes.MES ASC";
 
     $result = oci_parse($conn_ora, $querry);
     $valida = oci_execute($result);
